16d71f8a42ffa71c5f5d3f9fccd025a5f2bfaf00
Southbig <tesnam1216@gmail.com>
2022-11-16 13:48:09 +0900

    Next JS graphql API 생성

---
+++ nextjs-practice/pages/api/graphql.js
@@ -0,0 +1,33 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+
+const typeDefs = gql`
+    type Query {
+        users: [User!]!
+    }
+    type User {
+        name: String
+    }
+`;
+
+
+const resolvers = {
+  Query: {
+    users: (parent, args, context) => {
+      return [{ name: 'Nextjs' }];
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+module.exports = apolloServer.start().then(() => {
+  return apolloServer.createHandler({ path: '/api/graphql' });  // highlight-line
+});
+// export default apolloServer.createHandler({ path: '/api/graphql' });
